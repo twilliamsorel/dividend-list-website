7302cab934bdfac71eef40d1a8a4ebeabf65f89b
Trevor Sorel <twilliamsorel@gmail.com>
2023-07-02 13:30:38 -0600

    adding glossary/methodology

--- _src/_data/globals.js
+++ _src/_data/globals.js
@@ -3,10 +3,10 @@ module.exports = function () {
     environment: process.env.ENVIRONMENT || "development",
     cssPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main-min.css' : '/assets/css/main.css',
     jsPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main-min.js' : '/assets/js/main.js',
-    "title": "Dividend Research",
-    "description": "A simple tool for dividend stock discovery, focused on high yield income stocks that pay monthly.",
-    "server_address": "tbd",
-    "baseUrl": "https://dividendresearch.com",
+    "title": "Dividend List",
+    "description": "A tool for dividend stock discovery, focused on high yield income stocks that pay monthly.",
+    "server_address": "https://server.thedividendlist.com",
+    "baseUrl": "https://thedividendlist.com",
     "image": "/assets/img/logo.svg"
   };
 };
