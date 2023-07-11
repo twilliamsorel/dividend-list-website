1e6c5acc733d2dff5969285f30eb1f20a9fbe72d
Trevor Sorel <twilliamsorel@gmail.com>
2023-07-11 09:53:38 -0600

    fixing js path

--- _src/_data/globals.js
+++ _src/_data/globals.js
@@ -2,7 +2,7 @@ module.exports = function () {
   return {
     environment: process.env.ENVIRONMENT || "development",
     cssPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main-min.css' : '/assets/css/main.css',
-    jsPath: process.env.ENVIRONMENT === 'production' ? '/assets/js/main-min.js' : '/assets/js/main.js',
+    jsPath: process.env.ENVIRONMENT === '/assets/js/main.js',
     "title": "Dividend List",
     "description": "A tool for dividend stock discovery, focused on high yield income stocks that pay monthly.",
     "baseUrl": process.env.ENVIRONMENT === 'production' ? "https://thedividendlist.com" : "localhost:8080",
