874e776ec651fcb56473a507e928bd4c6cf98d79
Trevor Sorel <twilliamsorel@gmail.com>
2023-07-13 18:05:18 -0600

    quick fixes

--- _src/_data/globals.js
+++ _src/_data/globals.js
@@ -4,7 +4,7 @@ module.exports = function () {
     cssPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main-min.css' : '/assets/css/main.css',
     jsPath: '/assets/js/main.js',
     "title": "Dividend List",
-    "description": "A tool for dividend stock discovery, focused on high yield income stocks that pay monthly.",
+    "description": "A tool for dividend stock discovery, focused on high yield income stocks. The Dividend List tracks US stocks that have a monthly and quarterly dividend payout.",
     "baseUrl": process.env.ENVIRONMENT === 'production' ? "https://thedividendlist.com" : "localhost:8080",
     "image": "/assets/img/logo.svg"
   };
